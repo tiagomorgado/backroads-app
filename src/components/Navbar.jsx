8adc337e1240743345a74af94f89e9ac9e5beb1c
Tiago Morgado <Tiago.alcm@icloud.com>
2023-05-21 23:10:38 +0200

    restructured navbar comps

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import logo from '../images/logo.svg'
+import { pageLinks, socialLinks } from '../data'
 
 const Navbar = () => {
   return (
@@ -11,39 +12,30 @@ const Navbar = () => {
           </button>
         </div>
         <ul className="nav-links" id="nav-links">
-          <li>
-            <a href="#home" className="nav-link"> home </a>
-          </li>
-
-          <li>
-            <a href="#about" className="nav-link"> about </a>
-          </li>
-
-          <li>
-            <a href="#services" className="nav-link"> services </a>
-          </li>
-
-          <li>
-            <a href="#tours" className="nav-link"> tours</a>
-          </li>
+          {pageLinks.map((link) => {
+            return(
+              <li key={link.id}>
+                <a href={link.href} className='nav-link'>
+                  {link.text}
+                </a>
+              </li>
+            )
+          })}
         </ul>
 
         <ul className="nav-icons">
-          <li>
-            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-              ><i className="fab fa-facebook"></i
-            ></a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-              ><i className="fab fa-twitter"></i
-            ></a>
-          </li>
-          <li>
-            <a href="https://www.twitter.com" target="_blank" className="nav-icon"
-              ><i className="fab fa-squarespace"></i
-            ></a>
-          </li>
+          {
+            socialLinks.map((link) => {
+              const {id, href, icon} = link
+              return(
+                <li key={id}>
+                <a href={href} target='_blank' className='nav-icon'>
+                  <i className={icon}></i>
+                </a>
+              </li>
+              )
+            })
+          }
         </ul>
       </div>
     </nav>
